98d061e5efeb20b5790fea0cab1d578534cf539c
Azul Briones <you@example.com>
2022-12-21 15:22:18 -0600

    Fechas done

---
+++ Laravel/app/Filters/V1/FechaFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class FechaFilter extends ApiFilter {
+  protected $safeParms  = [
+    'proveedor_id'=>['eq'],
+    'mes_id' => ['eq'],
+    'dia_id'=>['eq'],
+    'hora_id' => ['eq'],
+    'disponibilidad'=>['eq']
+  ];
+
+  protected $columnMap = [
+    'proveedorId'=>'proveedor_id',
+    'mesId' => 'mes_id',
+    'diaId'=> 'dia_id',
+    'horaId' => 'hora_id'
+  ];
+
+protected $operatorMap = [
+  'eq' => '='
+];
+
+}
