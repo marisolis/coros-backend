20b23041d84500d129de7b49e292e0f5b5345d65
203417 <203417@ids.upchiapas.edu.mx>
2023-01-07 13:18:27 -0600

    contrato

--- Laravel/app/Http/Controllers/Api/V1/contratacionesController.php
+++ Laravel/app/Http/Controllers/Api/V1/contratacionesController.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Controller;
 class contratacionesController extends Controller
 {
     public function store(Request $request){
-        $Contrato = request(['id_paquete','nombre_paquete','id_cliente','nombre_cliente','Tipo_evento', 'Forma_de_pago', 'Fecha', 'Hora', 'Lugar','fecha_contrato','correo','correo_cliente','numero_telefono','id_proveedor']);
+        $Contrato = request(['paquete_id','nombre_paquete','usuario_id','nombre_cliente','Tipo_evento', 'Forma_de_pago', 'Fecha', 'Hora', 'Lugar','fecha_contrato','correo','correo_cliente','numero_telefono','empresa_id']);
         Contrataciones::create($Contrato);
 
         Mail::to($Contrato['correo'])->send(new correo($Contrato['nombre_cliente'], $Contrato['numero_telefono'],$Contrato['correo_cliente'], $Contrato['nombre_paquete'],$Contrato['Tipo_evento'],$Contrato['Lugar'],$Contrato['Fecha'],$Contrato['Hora']));
