90e1decc1adc7bcb5d4f7be2222b8a2a80330c86
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-02-18 14:16:38 -0600

    PUT en Contrataciones

--- Laravel/app/Http/Controllers/Api/V1/contratacionesController.php
+++ Laravel/app/Http/Controllers/Api/V1/contratacionesController.php
@@ -12,6 +12,7 @@ use App\Mail\correo;
 use App\Filters\V1\UserFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\UpdateContratacionesRequest;
+use Illuminate\Support\Facades\Log;
 
 class contratacionesController extends Controller
 {
@@ -22,15 +23,65 @@ class contratacionesController extends Controller
     }
 
     public function store(Request $request){
-        $Contrato = request(['paquete_id','nombre_paquete','usuario_id','nombre_cliente','Tipo_evento', 'Forma_de_pago', 'Fecha', 'Hora', 'Lugar','fecha_contrato','correo','correo_cliente','numero_telefono','empresa_id']);
-        Contrataciones::create($Contrato);
+        // $Contrato = $request(['paquete_id','nombre_paquete','usuario_id','nombre_cliente','Tipo_evento', 'Forma_de_pago', 'Fecha', 'Hora', 'Lugar','fecha_contrato','correo','correo_cliente','numero_telefono','empresa_id']);
+        
+        $new_contrato = new Contrataciones();
+        $new_contrato->paquete_id = $request->paquete_id;
+        // $new_contrato->nombre_paquete = $request->nombre_paquete;
+        $new_contrato->usuario_id = $request->usuario_id;
+        // $new_contrato->nombre_cliente = $request->nombre_cliente;
+        $new_contrato->Tipo_evento = $request->Tipo_evento;
+        $new_contrato->Forma_de_pago = $request->Forma_de_pago;
+        $new_contrato->Fecha = $request->Fecha;
+        $new_contrato->Hora = $request->Hora;
+        $new_contrato->Lugar = $request->Lugar;
+        $new_contrato->fecha_contrato = $request->fecha_contrato;
+        // $new_contrato->correo = $request->correo;
+        // $new_contrato->correo_cliente = $request->correo_cliente;
+        // $new_contrato->numero_telefono = $request->numero_telefono;
+        $new_contrato->empresa_id = $request->empresa_id;
 
-        Mail::to($Contrato['correo'])->send(new correo($Contrato['nombre_cliente'], $Contrato['numero_telefono'],$Contrato['correo_cliente'], $Contrato['nombre_paquete'],$Contrato['Tipo_evento'],$Contrato['Lugar'],$Contrato['Fecha'],$Contrato['Hora']));
+        $new_contrato->save();
+
+        // Contrataciones::create($Contrato);
+
+        // Mail::to($Contrato['correo'])->send(new correo($Contrato['nombre_cliente'], $Contrato['numero_telefono'],$Contrato['correo_cliente'], $Contrato['nombre_paquete'],$Contrato['Tipo_evento'],$Contrato['Lugar'],$Contrato['Fecha'],$Contrato['Hora']));
+
+        Mail::to($request->correo)->send(new correo($request->nombre_cliente, $request->numero_telefono,$request->correo_cliente, $request->nombre_paquete,$new_contrato->Tipo_evento,$new_contrato->Lugar,$new_contrato->Fecha,$new_contrato->Hora)
+        );
+
+        return response()->json([
+            'message' => 'Contrato creado correctamente'
+        ], 201);
     }
 
-    public function update(UpdateContratacionesRequest $request, Contrataciones $contrataciones
-    )
+    public function update(UpdateContratacionesRequest $request, $id){
+        // dd($id);
+        // dd($request->all());
+        $contratacion = Contrataciones::find($id);
+        // dd($contratacion);
+
+        $contratacion->paquete_id = $request->paquete_id;
+        // $contratacion->nombre_paquete = $request->nombre_paquete;
+        $contratacion->usuario_id = $request->usuario_id;
+        // $contratacion->nombre_cliente = $request->nombre_cliente;
+        $contratacion->Tipo_evento = $request->Tipo_evento;
+        $contratacion->Forma_de_pago = $request->Forma_de_pago;
+        $contratacion->Fecha = $request->Fecha;
+        $contratacion->Hora = $request->Hora;
+        $contratacion->Lugar = $request->Lugar;
+        $contratacion->fecha_contrato = $request->fecha_contrato;
+        // $contratacion->correo = $request->correo;
+        // $contratacion->correo_cliente = $request->correo_cliente;
+        // $contratacion->numero_telefono = $request->numero_telefono;
+        $contratacion->empresa_id = $request->empresa_id;
+        $contratacion->save();
+
+        return $contratacion;
+    }
+    public function show($id)
     {
-        $contrataciones->update($request->all());
+        $contratacion = Contrataciones::find($id);
+        return $contratacion;
     }
 }
